0d5e5786a2e343345e72296c5cc744defc761c08
Dovi Klausner <Dovi.Klausner@amdocs.com>
2019-03-17 16:20:01 +0200

    update

--- search_regex.py
+++ search_regex.py
@@ -9,33 +9,47 @@ import os
 def parse_parameters():
     """Pars the command line parameters and return the args dictionary."""
     parser = argparse.ArgumentParser(description="Search Regex Tool")
-    parser.add_argument('-r', '--regex',
-                        action="store",
-                        help="regex",
-                        required=True)
-    parser.add_argument('-f', '--files', 
-                        type=argparse.FileType('r'), 
-                        nargs='*',
-                        help="List of files, separated by space, default is STDIN",
-                        default=[sys.stdin])
+    parser.add_argument('-r', '--regex', action="store",
+                        help="Regular expression to search in files/STDIN", required=True)
+    parser.add_argument('-f', '--files', type=argparse.FileType('r'), 
+                        nargs='*', default=[sys.stdin],
+                        help="List of files, separated by space, default is STDIN")
+    parser.add_argument('-u', '--underscore', action="store_true",
+                        help="Prints '^' under the matching text. Default false",
+                        default=False)
+    parser.add_argument('-c', '--color', action="store_true",
+                        help="Highlight matching text. Default false",
+                        default=False)
+    parser.add_argument('-m', '--machine', action="store_true",
+                        help="Generate machine readable output. Default false",
+                        default=False)
 
     args = parser.parse_args()
     return args
 
 
-def search_regex(reg, file):
+def search_regex(reg, file, machine, underscore, color):
     pattern = re.compile(reg)
     for i, line in enumerate(file):
         for match in re.finditer(pattern, line.strip()):
-            print 'Found on line %s, column %s : %s' % (i+1, match.start(0)+1, line.strip())
-    
+            if color:
+                text = line[:match.start(0)] + "\033[31m" + line[match.start(0):match.end(0)] + "\033[m" + line[match.end(0):]
+            else:
+                text = line
+            if machine:
+                print '%s:%s:%s:%s' % (file.name, i+1, match.start(0)+1, text.strip())
+            else:                    
+                print 'Found in file %s, on line %s, column %s : %s' % (file.name, i+1, match.start(0)+1, text.strip())
+
 
 def main():
     args = parse_parameters()
+    #print args
     #print args.regex
     for f in args.files:
         #print f
-        search_regex(args.regex,f)
+        search_regex(args.regex,f, args.machine, 
+                    args.underscore, args.color)
 
 
 if __name__ == '__main__':
