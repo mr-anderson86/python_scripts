446679bb791ff294e434c5b10c06ce7de280d7ec
Dovi Klausner <Dovi.Klausner@amdocs.com>
2019-03-17 13:45:53 +0200

    added script

---
+++ search_regex.py
@@ -0,0 +1,42 @@
+#!/bin/env python
+
+import sys, re
+import traceback
+import argparse
+import logging
+import os
+
+def parse_parameters():
+    """Pars the command line parameters and return the args dictionary."""
+    parser = argparse.ArgumentParser(description="Search Regex Tool")
+    parser.add_argument('-r', '--regex',
+                        action="store",
+                        help="regex",
+                        required=True)
+    parser.add_argument('-f', '--files', 
+                        type=argparse.FileType('r'), 
+                        nargs='*',
+                        help="List of files, separated by space, default is STDIN",
+                        default=[sys.stdin])
+
+    args = parser.parse_args()
+    return args
+
+
+def search_regex(reg, file):
+    pattern = re.compile(reg)
+    for i, line in enumerate(file):
+        for match in re.finditer(pattern, line.strip()):
+            print 'Found on line %s: %s' % (i+1, line)
+    
+
+def main():
+    args = parse_parameters()
+    #print args.regex
+    for f in args.files:
+        #print f
+        search_regex(args.regex,f)
+
+
+if __name__ == '__main__':
+    main()
